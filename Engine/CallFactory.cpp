381cceaaa78015c45d6646f9b6c091cb9c501b65
heller77 <koki001230@icloud.com>
2023-09-17 23:11:24 +0900

    動的にDLLを読み込むようにした

--- Engine/CallFactory.cpp
+++ Engine/CallFactory.cpp
@@ -1,8 +1,22 @@
 #include "./CallFactory.h"
 #include "./../Factory/Factory.h"
 #include <iostream>
+#include "windows.h"
+typedef Component *(CALLBACK *LPFNDLLFUNC1)(std::string component_name);
+
 Component *CallFactory::Generate(std::string component_name)
 {
-    std::cout << "generate call from CallFactory" << std::endl;
-    return GenerateComponent(component_name);
+    auto factoryDLL = LoadLibrary("FactoryDLL");
+    if (factoryDLL != NULL)
+    {
+        auto func = (LPFNDLLFUNC1)GetProcAddress(factoryDLL, "GenerateComponent");
+        auto component = func(component_name);
+
+        return component;
+    }
+    else
+    {
+        std::cout << "not fine dll" << std::endl;
+        return nullptr;
+    }
 }
